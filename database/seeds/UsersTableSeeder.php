32c6496e3082644ec43049f3fb82621ab07e813a
aranej <araneta.j@cambridgeassessment.org.uk>
2019-05-07 22:10:39 +0800

    added user table seeder

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -4,9 +4,17 @@ use Illuminate\Support\Str;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 use App\Models\User;
+use App\Models\CredentialsGenerator;
 
 class UsersTableSeeder extends Seeder
 {
+
+	protected $credGen;
+
+	public function __construct(CredentialsGenerator $credGen) {
+        $this->credGen = $credGen;
+    }
+
     /**
      * Run the database seeds.
      *
@@ -14,6 +22,15 @@ class UsersTableSeeder extends Seeder
      */
     public function run()
     {
-        //
+        DB::table('users')->insert([
+            'username' => $this->credGen->generateUsername('John Jeiel', 'Noblejas', 'Araneta'),
+            'first_name' => 'John Jeiel',
+            'middle_name' => 'Noblejas',
+            'last_name' => 'Araneta',
+            'contact_number' => '09081858655',
+            'email' => 'jeielaraneta@gmail.com',
+            'password' => bcrypt('secret123'),
+        ]);
+
     }
 }
