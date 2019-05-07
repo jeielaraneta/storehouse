9041534b67056ec8cdc5f95782806fb3a7dc792f
aranej <araneta.j@cambridgeassessment.org.uk>
2019-05-07 21:59:56 +0800

    added home page for members

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -52,14 +52,15 @@ class RegisterController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function register(Request $request)
+    
+    /*public function register(Request $request)
     {
         $this->validator($request->all())->validate();
 
         event(new Registered($user = $this->create($request->all())));
 
         return $this->registered($request, $user) ?: redirect($this->redirectPath());
-    }
+    }*/
 
     /**
      * Get a validator for an incoming registration request.
--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,9 @@ Route::get('/', function () {
 Auth::routes(['verify' => true]);
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::prefix('/member')->group(function () {
+
+    Route::get('/', 'MemberController@index')->name('member-index');
+
+});
