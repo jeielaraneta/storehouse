9d7e9bc4eb2c1d59734df82329556d1f8cb19669
aranej <araneta.j@cambridgeassessment.org.uk>
2019-09-03 17:52:15 +0800

    view users table implemented

--- app/Models/User.php
+++ app/Models/User.php
@@ -46,4 +46,12 @@ class User extends Authenticatable implements MustVerifyEmail
     {
         $this->notify(new Notifications\VerifyUser);
     }*/
+
+    public function getCreatedAtAttribute($value) {
+        return date('m-d-Y h:i A', strtotime($value));
+    }
+
+    public function getEmailVerifiedAtAttribute($value) {
+        return date('m-d-Y h:i A', strtotime($value));
+    }
 }
--- resources/views/admin/users/user.blade.php
+++ resources/views/admin/users/user.blade.php
@@ -15,9 +15,50 @@
             <div class="card">
                 <div class="card-header bg-transparent border-primary"><h5>List of Users</h5></div>
                 <div class="card-body">
-                    Coming soon!
 
-                    {{ $users }}
+                    <div class="table-responsive">
+                        <table id="usersTable" class="table table-bordered table-striped display cell-border" style="width:100%">
+                            <thead>
+                                <tr>
+                                    <th>Name</th>
+                                    <th>Role</th>
+                                    <th>Email</th>
+                                    <th>Date Created</th>
+                                    <th>Date Verified</th>
+                                    <th>Password Reset</th>
+                                    <th>Status</th>
+                                    <th>Actions</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                @if ( count($users) > 0 )
+                                    @foreach ($users as $user)
+                                        <tr>
+                                            <td>{{$user->first_name}} {{$user->last_name}}</td>
+                                            <td></td>
+                                            <td>{{$user->email}}</td>
+                                            <td>{{$user->created_at}}</td>
+                                            <td>{{$user->email_verified_at}}</td>
+                                            <td></td>
+                                            <td></td>
+                                            <td><show-button route="{{ route('user.show', $user->id)}}" btn-text="View"></show-button></td>
+                                        </tr>
+                                    @endforeach
+                                @endif
+                            </tbody>
+                            <tfoot>
+                                <tr>
+                                    <th>Name</th>
+                                    <th>Role</th>
+                                    <th>Email</th>
+                                    <th>Date Created</th>
+                                    <th>Date Verified</th>
+                                    <th>Password Reset</th>
+                                    <th>Status</th>
+                                    <th>Actions</th>
+                            </tfoot>
+                        </table>
+                    </div>
                 </div>
             </div>
         </div>
